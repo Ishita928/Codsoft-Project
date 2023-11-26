27f89d94f944ae0d20663a298d89346570cf356f
Ishita Malhotra <146575485+Ishita928@users.noreply.github.com>
2023-11-27 02:30:02 +0530

    Add files via upload

---
+++ calculator(task3)/script.js
@@ -0,0 +1,42 @@
+
+let screen = document.getElementById('display');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        if (buttonText == 'x') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == '÷') {
+            buttonText = '/';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText === 'DEL') {
+            screenValue =  screenValue.slice(0, -1);
+            screen.value = screenValue;
+        }
+        
+
+        else if (buttonText == 'Clear') {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+            screenValue=" ";
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    })
+}
+
+
+
